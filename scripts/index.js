cd4e0fe506c282b2fd499a51544b5effa711e754
Alex Timsoshenko <alex.team220986@yandex.ru>
2020-10-14 15:57:12 +0300

    14.10.qqq1

--- scripts/index.js
+++ scripts/index.js
@@ -6,25 +6,24 @@ let nameInput = document.querySelector('#title');
 let profileTitle = document.querySelector(".profile__title");
 let jobInput = document.querySelector('#subtitle');
 let profileSubtitle = document.querySelector(".profile__subtitle");
-let saveButton = document.querySelector('.popup__button-safe');
 
 function openPopup () {
     nameInput.value = profileTitle.textContent;
     jobInput.value = profileSubtitle.textContent;
     popup.classList.toggle("popup_is-opened");
-};
+}
 
 function closePopup () {
     popup.classList.toggle("popup_is-opened");
-};
+}
 
 function formSubmitHandler (evt) {
     evt.preventDefault(); 
     profileTitle.textContent = nameInput.value;
     profileSubtitle.textContent = jobInput.value;
-    popup.classList.toggle("popup_is-opened");
+    closePopup ();
 }
 
 buttonOpenPopup.addEventListener("click", openPopup);
 buttonClosePopup.addEventListener("click", closePopup);
-saveButton.addEventListener('click', formSubmitHandler);
+form.addEventListener('submit', formSubmitHandler);
